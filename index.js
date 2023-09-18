27eaf5ec8cd87cfcaec7993c7a20bc1e011fc8c1
canbeyour <wuzucy@gmail.com>
2023-09-18 22:57:16 +0900

    feat: add video using webcam

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-let cv; // OpenCV 객체
+let cv; // OpenCV 객체]
+let video;
 
 function onOpenCvReady() {
   cv = window.cv; // OpenCV 객체 초기화
   if (cv) {
     console.log("OpenCV.js is ready");
+    initialize();
   } else {
     console.error("OpenCV.js failed to load.");
+  }
+}
+
+async function initialize() {
+  video = document.getElementById("webcam");
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.srcObject = stream;
+  } catch (e) {
+    console.error(e);
   }
 }

56a086b24ebdcbd967a7cdbb03e4b993da620e13
canbeyour <wuzucy@gmail.com>
2023-09-25 14:01:52 +0900

    feat: add function to set images

--- canvas.js
+++ canvas.js
@@ -1,2 +1,19 @@
 const images = [];
 const imagePaths = ["./rock.jpg", "./scissor.jpg", "./paper.jpg"];
+
+function setImages() {
+  for (let i = 0; i < 10; i++) {
+    let num = Math.floor(Math.random() * imagePaths.length);
+
+    let x = Math.floor(Math.random() * canvas.width - imageWidth);
+    let y = Math.floor(Math.random() * 800) * -1;
+    let speed = 3;
+
+    images.push({
+      x,
+      y,
+      speed,
+      imagePath: imagePaths[num],
+    });
+  }
+}
